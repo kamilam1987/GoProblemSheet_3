ee49849fc7fbffe41d28bc3931d2b913d422be9d
unknown <kamilam1987@gmail.com>
2017-12-01 21:02:47 +0000

    question 6

--- ProblemGo3.go
+++ ProblemGo3.go
@@ -1,10 +1,11 @@
+//Author: Kmila Michel g00340498
+//Problem sheet 3 : Problem set: Regular expressions
 package main
 
 import (
 	"fmt"
 	"math/rand"
 	"regexp"
-	"strings"
 	"time"
 )
 
@@ -12,6 +13,15 @@ func ElizaResponse(input string) string {
 	//\b word boundry
 	if matched, _ := regexp.MatchString(`(?i).*\bfather\b.*`, input); matched {
 		return "Why don’t you tell me more about your father"
+	}
+	if matched, _ := regexp.MatchString(`(.*) ?remember ?(.*)`, input); matched {
+		return "Can you tell me more ?"
+	}
+	if matched, _ := regexp.MatchString(`(.*) ?Hello?(.*)`, input); matched {
+		return "Hello it's nice to meet you"
+	}
+	if matched, _ := regexp.MatchString(`(.*) ?sorry?(.*)`, input); matched {
+		return "Why should I be?"
 	}
 
 	re := regexp.MustCompile(`(?i)I am ([^.?!].*)[.?!]?`)
@@ -21,66 +31,44 @@ func ElizaResponse(input string) string {
 		return re.ReplaceAllString(input, "How do you know you are $1?")
 	}
 
+	//Array of random answers
 	answers := []string{
 		"I’m not sure what you’re trying to say. Could you explain it to me?",
 		"How does that make you feel?",
 		"Why do you say that?",
 	}
+
 	//Return random string from answers array
 	return answers[rand.Intn(len(answers))]
 }
-func Reflect(input string) string {
-	// Split the input on word boundaries.
-	boundaries := regexp.MustCompile(`\b`)
-	words := boundaries.Split(input, -1)
-
-	// List the reflections.
-	reflections := [][]string{
-		{`I`, `you`},
-		{`i`, `you`},
-		{`I`, `You`},
-		{`am`, `are`},
-		{`was`, `were`},
-		{`i'd`, `you would`},
-		{`i've`, `you have`},
-		{`i'll`, `you will`},
-		{`are`, `am`},
-		{`you've`, `I have`},
-		{`you'll`, `I will`},
-		{`yours`, `mine`},
-		{`me`, `you`},
-		{`you`, `me`},
-		{`my`, `your`},
-		{`your`, `my`},
-	}
-
-	// Loop through each token, reflecting it if there's a match.
-	//Code adapted from : https://gist.github.com/ianmcloughlin/c4c2b8dc586d06943f54b75d9e2250fe
-	for i, word := range words {
-		for _, reflection := range reflections {
-			if matched, _ := regexp.MatchString(reflection[0], word); matched {
-				words[i] = reflection[1]
-				break
-			}
-		}
-	}
-
-	// Put the tokens back together.
-	return strings.Join(words, ``)
-} //End of Reflect function
 
 func main() {
-	//Comments
+
 	//Prints random input and output from eliza that matched string
 	rand.Seed(time.Now().Unix())
 	fmt.Println("People say I look like both my mother and father.")
 	fmt.Println(ElizaResponse("People say I look like both my mother and father."))
+	fmt.Println()
+	fmt.Println("Hello.")
+	fmt.Println(ElizaResponse("Hello."))
+	fmt.Println()
+	fmt.Println("Are you a computer?")
+	fmt.Println(ElizaResponse("Are you a computer?"))
+	fmt.Println()
+	fmt.Println("Are you sorry?")
+	fmt.Println(ElizaResponse("Are you sorry?"))
+	fmt.Println()
+	fmt.Println("I remember when I was young.")
+	fmt.Println(ElizaResponse("I remember when I was young."))
 	fmt.Println()
 	fmt.Println("Father was a teacher")
 	fmt.Println(ElizaResponse("Father was a teacher"))
 	fmt.Println()
 	fmt.Println("I was my father’s favourite")
 	fmt.Println(ElizaResponse("I was my father’s favourite!"))
+	fmt.Println()
+	fmt.Println("I like my friend")
+	fmt.Println(ElizaResponse("I like my friend!"))
 	fmt.Println()
 	fmt.Println("I’m looking forward to the weekend.")
 	fmt.Println(ElizaResponse("I’m looking forward to the weekend."))
