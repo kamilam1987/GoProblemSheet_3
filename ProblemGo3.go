fff9b4512dc27974e676c1f73806ca952d9737e5
unknown <kamilam1987@gmail.com>
2017-12-01 00:14:18 +0000

    First commit

---
+++ ProblemGo3.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"regexp"
+	"time"
+)
+
+func ElizaResponse(input string) string {
+	//\b word boundry
+	if matched, _ := regexp.MatchString(`(?i).*\bfather\b.*`, input); matched {
+		return "Why don’t you tell me more about your father"
+	}
+
+	re := regexp.MustCompile(`(?i)I am ([^.?!].*)[.?!]?`)
+	//re := regexp.MustCompile(`(?i).*\bI'?\s*a?m \b([^.?!]*)[.?!]?`)
+	if matched := re.MatchString(input); matched {
+
+		return re.ReplaceAllString(input, "How do you know you are $1?")
+	}
+
+	answers := []string{
+		"I’m not sure what you’re trying to say. Could you explain it to me?",
+		"How does that make you feel?",
+		"Why do you say that?",
+	}
+	//Return random string from answers array
+	return answers[rand.Intn(len(answers))]
+}
+
+func main() {
+
+	//Prints random input and output from eliza that matched string
+	rand.Seed(time.Now().Unix())
+	fmt.Println("People say I look like both my mother and father.")
+	fmt.Println(ElizaResponse("People say I look like both my mother and father."))
+	fmt.Println()
+	fmt.Println("Father was a teacher")
+	fmt.Println(ElizaResponse("Father was a teacher"))
+	fmt.Println()
+	fmt.Println("I was my father’s favourite")
+	fmt.Println(ElizaResponse("I was my father’s favourite!"))
+	fmt.Println()
+	fmt.Println("I’m looking forward to the weekend.")
+	fmt.Println(ElizaResponse("I’m looking forward to the weekend."))
+	fmt.Println()
+	fmt.Println("My grandfather was French!")
+	fmt.Println(ElizaResponse("My grandfather was French!"))
+	fmt.Println()
+
+	fmt.Println("I am happy.")
+	fmt.Println(ElizaResponse("I am happy."))
+	fmt.Println()
+	fmt.Println("I am not happy with your responses")
+	fmt.Println(ElizaResponse("I am not happy with your responses"))
+	fmt.Println()
+	fmt.Println("I am not sure that you understand the effect that your questions are having on me.")
+	fmt.Println(ElizaResponse("I am not sure that you understand the effect that your questions are having on me."))
+	fmt.Println()
+}
